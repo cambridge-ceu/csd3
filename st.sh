615979133fce38d88596badd3a011b461a3d4c41
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2019-07-05 13:47:53 +0100

    batch

---
+++ st.sh
@@ -0,0 +1,10 @@
+git add README.md
+git commit -m "README"
+git remote add origin git@github.com:cambridge-ceu/csd3.git
+git add csd3-software-list.tsv
+git commit -m "software wish list"
+git add reinstall_r_pkgs.R
+git commit -m "reinstall_r_pkgs.R"
+git add st.sh
+git commit -m "batch"
+git push -u origin master
