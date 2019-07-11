24e2885e50c1ee647f26c069639ecb39210ebd73
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2019-07-11 08:50:30 +0100

    batch

--- st.sh
+++ st.sh
@@ -1,10 +1,8 @@
-# 8-7-2019 JHZ
+# 11-7-2019 JHZ
 
 git add README.md
 git commit -m "README"
 git remote add origin git@github.com:cambridge-ceu/csd3.git
-git add csd3-software-list.tsv
-git commit -m "software wish list"
 git add reinstall_r_pkgs.R
 git commit -m "reinstall_r_pkgs.R"
 git add usage.md
