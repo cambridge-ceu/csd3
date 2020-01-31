473f636900604aa80508aa4c882da3b8336fee4d
jinghuazhao <jinghuazhao@hotmail.com>
2020-01-31 09:18:44 +0000

    batch

--- st.sh
+++ st.sh
@@ -1,14 +1,16 @@
-# 20-7-2019 JHZ
+# 31-7-2019 JHZ
 
 git add files
 git commit -m "copies of files"
 git add README.md
 git commit -m "README"
+git add cardio.md
+git commit -m "Information on Cardio"
 git remote add origin git@github.com:cambridge-ceu/csd3.git
 git add reinstall_r_pkgs.R
 git commit -m "reinstall_r_pkgs.R"
 git add usage.md
-git commit -m "Usage examples"
+git commit -m "Usage notes"
 git add st.sh
 git commit -m "batch"
 git push -u origin master
