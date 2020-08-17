752edf87c858b20aa6ea529298d187b8a53592fb
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2020-08-17 23:17:16 +0100

    copies of files

---
+++ files/st.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/bash
+
+git add docs
+git commit -m "docs"
+git add files
+git commit -m "copies of files"
+git add README.md
+git commit -m "README"
+git add cardio.md
+git commit -m "Information on Cardio"
+git remote add origin git@github.com:cambridge-ceu/csd3.git
+git add reinstall_r_pkgs.R
+git commit -m "reinstall_r_pkgs.R"
+git add usage.md
+git commit -m "Usage notes"
+git push -u origin master
