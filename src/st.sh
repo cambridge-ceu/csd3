7bdf4c55cdbb1af695c9e021abfbab4b6c3a50aa
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2021-02-14 13:21:52 +0000

    commands

--- src/st.sh
+++ src/st.sh
@@ -1,18 +1,12 @@
 #!/usr/bin/bash
 
-git add cardio
-git commit -m "information on CEU's Cardio"
+# git remote add origin git@github.com:cambridge-ceu/csd3.git
 git add docs
 git commit -m "docs"
 git add src
 git commit -m "commands"
 git add README.md
 git commit -m "README"
-git add cardio.md
-git commit -m "Information on Cardio"
-git remote add origin git@github.com:cambridge-ceu/csd3.git
 git add usage.md
 git commit -m "Usage notes"
-git add _config.yml
-git commit -m "_config.yml"
 git push -u origin master
