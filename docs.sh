4dab6f298e36d344370545b349712a864953c9bb
Jing Hua Zhao <jinghuazhao@hotmail.com>
2022-04-07 21:18:56 +0100

    docs.sh

--- docs.sh
+++ docs.sh
@@ -2,7 +2,8 @@
 
 function renum()
 {
-  ls applications/*md | grep -v -e README -e files | sort | xargs -I {} basename {} .md | awk '{print NR,$1}' | \
+  ls applications/*md  | \
+  grep -v -e README -e files | sort | xargs -I {} basename {} .md | awk '{print NR,$1}' | \
   parallel -j1 -C' ' '
     echo {1} {2}
     export line=2
